500d0ea2cef5932c7dc7b6999afe48ebdb1782c0
Will Reed <wreedb@yandex.com>
2022-06-30 21:52:01 -0500

    added better help info

--- kaylee.nim
+++ kaylee.nim
@@ -3,6 +3,8 @@ import crappycli;
 import std/strutils, strformat;
 import os 
 
+let helpstr: string = &"\nKaylee | AUR helper written in Nim\n  s => search for a package\n  i => install a package\n  u => update packages\n  r => remove a package\n  q => query installed packages";
+
 if not existsOrCreateDir(&"{homeDir}/.cache"):
   quit(1)
 if not existsOrCreateDir(&"{homeDir}/.cache/kaylee"):
@@ -19,17 +21,17 @@ if not fileExists(fileloc):
 let cli = newCrappyCli()
 
 if cli.empty: 
-  echo "no argument given\n use --help for more information"
+  echo "no argument given\n use 'kaylee help' for more information"
   quit(1)
 
 var firstarg = toLowerAscii(cli.first);
 let secondarg = toLowerAscii(cli.second);
 
 
-const opts = ["s", "u", "i", "q", "r"]
+const opts = ["s", "u", "i", "q", "r", "h", "help"]
 
 if not opts.contains(firstarg):
-  echo &"kaylee doesn't know the command {firstarg}\n use --help to see available commands"
+  echo &"kaylee doesn't know the command {firstarg}\n use 'kaylee help' to see available commands"
 
 
 proc emptyargs(secondarg: string): string =
@@ -59,3 +61,7 @@ case firstarg:
       createMyYaml();
       removePackage(emptyargs(secondarg));
       discard updatepkglist();
+  of "help":
+    echo helpstr;
+  of "h":
+    echo helpstr;
